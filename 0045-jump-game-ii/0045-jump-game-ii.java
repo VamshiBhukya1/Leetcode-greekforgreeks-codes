6a54662eeffd1a0b0e243c5cf8924c00697cff1f
Vamshi Bhukya <23951A66N8@iare.ac.in>
2026-02-19 22:21:24 +0530

    Time: 1 ms (99.56%), Space: 47.6 MB (18.8%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0;
+        int l=0;
+        int r=0;
+        int n=nums.length;
+        while(r<n-1){
+            int far=0;
+            for(int ind=l;ind<=r;ind++){
+                far=Math.max(ind+nums[ind],far);
+            }
+                l=r+1;
+                r=far;
+                jumps++;
+        }
+        return jumps;
+        
+    }
+}
